a9eba5a2b6fc8ed781233cdec3f38f87d67c760d
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-08 11:43:08 +0200

    reimplemented using streams with side effects (incorrect)

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,15 +1,18 @@
 package com.solutions.codility;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class Solution {
     public int solution(int[] A) {
-        int[] B = IntStream.of(A).filter(x -> x > 0).sorted().distinct().toArray();
-        for (int i = 0; i < B.length; i++) {
-            if (i + 1 != B[i]) {
-                return i + 1;
+        AtomicInteger result = new AtomicInteger(1);
+
+        //Fancy stream-only way, potentially buggy, and anti-functional (in functional programming we avoid side effects, whereas here we utilize a side-effect of rewriting result)
+        IntStream.of(A).filter(x -> x > 0).sorted().distinct().forEachOrdered(i -> {
+            if (i == result.get()) {
+                result.set(i + 1);
             }
-        }
-        return B.length + 1;
+        });
+        return result.get();
     }
 }
