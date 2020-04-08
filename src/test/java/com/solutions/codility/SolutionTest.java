d38e13765cd9f7e36f429fd3b4d68a8c44687ac9
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-08 11:12:18 +0200

    implemented more tests

--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -22,8 +22,68 @@ public class SolutionTest {
     }
 
     @Test
-    public void testOnBiggerArrays() {
-        assertEquals(5, new Solution().solution(new int[]{1, 2, 3, 4}));
+    public void testOnBiggerContinuousArray() {
+        final int size = 100000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - i;
+        }
+        assertEquals(size + 1, new Solution().solution(A));
     }
 
+    @Test
+    public void testOnBiggerLastItem() {
+        final int size = 100000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - i;
+        }
+
+        int gapIndex = 2;
+        int expectedResult = A[gapIndex];
+        A[gapIndex] = -1;
+        assertEquals(expectedResult, new Solution().solution(A));
+    }
+
+    @Test
+    public void testOnBiggerArrayAlmostLast() {
+        final int size = 100000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - i;
+        }
+        int gapIndex = size / 4;
+        int expectedResult = A[gapIndex];
+        A[gapIndex] = -1;
+        assertEquals(expectedResult, new Solution().solution(A));
+    }
+
+    @Test
+    public void testOnBiggerArrayHalfWay() {
+        final int size = 100000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - i;
+        }
+
+        int gapIndex = size / 2;
+        int expectedResult = A[gapIndex];
+        A[gapIndex] = -1;
+        assertEquals(expectedResult, new Solution().solution(A));
+
+    }
+
+    @Test
+    public void testOnBiggerArrayInBeginning() {
+        final int size = 100000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - i;
+        }
+
+        int gapIndex = 3 * size / 4;
+        int expectedResult = A[gapIndex];
+        A[gapIndex] = -1;
+        assertEquals(expectedResult, new Solution().solution(A));
+    }
 }
