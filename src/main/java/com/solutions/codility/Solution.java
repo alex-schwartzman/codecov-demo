bce49e5db50233e5ecc096deb796f209d1e27781
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-08 11:00:13 +0200

    implemented solution

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,7 +1,22 @@
 package com.solutions.codility;
 
+import java.util.Arrays;
+
 public class Solution {
     public int solution(int[] A) {
-        return 42;
+        Arrays.sort(A);
+        int result = 1;
+        for (int i : A) {
+            if (i < result) {
+                continue;
+            }
+            else if (i == result) {
+                result++;
+            }
+            else if (i > result) {
+                break;
+            }
+        }
+        return result;
     }
 }
