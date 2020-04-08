6434cc95c23efc43fefd19dc72316fbde953341b
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-08 10:56:11 +0200

    sample codility task test definition

--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -7,15 +7,23 @@ import static org.junit.Assert.assertEquals;
 public class SolutionTest {
 
     @Test
-    public void testAsDescribedInTask(){
+    public void testAsDescribedInTask1() {
         assertEquals(5, new Solution().solution(new int[]{1, 3, 6, 4, 1, 2}));
-        assertEquals(4, new Solution().solution(new int[]{1,2,3}));
-        assertEquals(1, new Solution().solution(new int[]{-1,-3}));
-     }
+    }
 
     @Test
-    public void testOnBiggerArrays(){
-        assertEquals(5, new Solution().solution(new int[]{1,2,3,4}));
+    public void testAsDescribedInTask2() {
+        assertEquals(4, new Solution().solution(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void testAsDescribedInTask3() {
+        assertEquals(1, new Solution().solution(new int[]{-1, -3}));
+    }
+
+    @Test
+    public void testOnBiggerArrays() {
+        assertEquals(5, new Solution().solution(new int[]{1, 2, 3, 4}));
     }
 
 }
