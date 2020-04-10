ffb53bd65c184b00f1e071befa7e85a97e18c20b
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-10 22:06:17 +0200

    solution for sprinklers arrangement https://app.codility.com/programmers/task/sprinklers_arrangement/

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,22 +1,161 @@
 package com.solutions.codility;
 
-import java.util.Arrays;
+import java.util.*;
 
+//https://app.codility.com/programmers/task/sprinklers_arrangement/
 public class Solution {
-    public int solution(int[] A) {
-        Arrays.sort(A);
-        int result = 1;
-        for (int i : A) {
-            if (i < result) {
-                continue;
-            }
-            else if (i == result) {
-                result++;
-            }
-            else if (i > result) {
-                break;
+    public static class Coord {
+        public int column, row;
+
+        public Coord(int column, int row) {
+            this.column = column;
+            this.row = row;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Coord)) return false;
+            Coord coord = (Coord) o;
+            return column == coord.column &&
+                    row == coord.row;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(column, row);
+        }
+    }
+
+    int stepsCount = 0;
+    Set<Coord> sprinklers = new HashSet<>();
+    Map<Integer, Set<Integer>> rows = new HashMap<>();
+    Map<Integer, Set<Integer>> columns = new HashMap<>();
+    int fieldSize;
+
+    public int solution(int[] X, int[] Y) {
+        fieldSize = X.length;
+
+        initColumnsRows(fieldSize);
+
+        for (int i = 0; i < fieldSize; i++) {
+            addSprinkler(new Coord(X[i] - 1, Y[i] - 1));
+        }
+
+        //walk through columns and move them left and right to get even distribution
+        for (int x = 0; x < fieldSize; x++) {
+            Set<Integer> currentColumn = columns.get(x);
+            while (currentColumn.size() != 1) {
+                if (currentColumn.size() > 1) {
+                    Coord sprinklerToBeMoved = new Coord(x, currentColumn.iterator().next()); //?
+                    int nextEmptyColumnOffset = findNextEmptyColumnOffset(x);
+                    moveSprinklerRight(sprinklerToBeMoved, nextEmptyColumnOffset);
+                    stepsCount += nextEmptyColumnOffset;
+                } else {
+                    int nextSprinklerColumnOffset = findNextSprinklerColumnOffset(x);
+                    int row = columns.get(x + nextSprinklerColumnOffset).iterator().next();
+                    Coord from = new Coord(x + nextSprinklerColumnOffset, row);
+                    moveSprinklerRight(from, -nextSprinklerColumnOffset);
+                    stepsCount += nextSprinklerColumnOffset;
+                }
             }
         }
-        return result;
+
+        //walk through rows and move them left and right to get even distribution
+        for (int y = 0; y < fieldSize; y++) {
+            Set<Integer> currentRow = rows.get(y);
+            while (currentRow.size() != 1) {
+                if (currentRow.size() > 1) {
+                    Coord sprinklerToBeMoved = new Coord(currentRow.iterator().next(), y); //?
+                    int nextEmptyRowOffset = findNextEmptyRowOffset(y);
+                    moveSprinklerDown(sprinklerToBeMoved, nextEmptyRowOffset);
+                    stepsCount += nextEmptyRowOffset;
+                } else {
+                    int nextSprinklerRowOffset = findNextSprinklerRowOffset(y);
+                    int column = rows.get(y + nextSprinklerRowOffset).iterator().next();
+                    Coord from = new Coord(column, y + nextSprinklerRowOffset);
+                    moveSprinklerDown(from, -nextSprinklerRowOffset);
+                    stepsCount += nextSprinklerRowOffset;
+                }
+            }
+        }
+
+        return stepsCount;
     }
+
+    private int findNextSprinklerColumnOffset(int columnToStart) {
+        for (int column = columnToStart + 1; column < fieldSize + 1; column++) {
+            if (columns.get(column).size() > 1) {
+                return column - columnToStart;
+            }
+        }
+        System.out.println("impossible condition - sprinklers count less than field size (findNextSprinklerColumnOffset)");
+        return 0;
+    }
+
+    private int findNextEmptyColumnOffset(int columnToStart) {
+        for (int column = columnToStart + 1; column < fieldSize + 1; column++) {
+            if (columns.get(column).isEmpty()) {
+                return column - columnToStart;
+            }
+        }
+        System.out.println("impossible condition - sprinklers count less than field size (findNextEmptyColumnOffset)");
+        return fieldSize;
+    }
+
+    private int findNextSprinklerRowOffset(int rowToStart) {
+        for (int row = rowToStart + 1; row < fieldSize + 1; row++) {
+            if (rows.get(row).size() > 1) {
+                return row - rowToStart;
+            }
+        }
+        System.out.println("impossible condition - sprinklers count less than field size (findNextSprinklerRowOffset)");
+        return 0;
+    }
+
+    private int findNextEmptyRowOffset(int rowToStart) {
+        for (int row = rowToStart + 1; row < fieldSize + 1; row++) {
+            if (rows.get(row).isEmpty()) {
+                return row - rowToStart;
+            }
+        }
+        System.out.println("impossible condition - sprinklers count less than field size (findNextEmptyRowOffset)");
+        return 0;
+    }
+
+    private void initColumnsRows(int fieldSize) {
+        for (int i = 0; i < fieldSize + 1; i++) {
+            columns.put(i, new HashSet<>());
+            rows.put(i, new HashSet<>());
+        }
+    }
+
+    private void addSprinkler(Coord coord) {
+        sprinklers.add(coord);
+        columns.get(coord.column).add(coord.row);
+        rows.get(coord.row).add(coord.column);
+    }
+
+    private void removeSprinkler(Coord coord) {
+        sprinklers.remove(coord);
+        columns.get(coord.column).remove(coord.row);
+        rows.get(coord.row).remove(coord.column);
+    }
+
+
+    private void moveSprinkler(Coord from, Coord to) {
+        removeSprinkler(from);
+        addSprinkler(to);
+    }
+
+    private void moveSprinklerDown(Coord from, int count) {
+        Coord to = new Coord(from.column, from.row + count);
+        moveSprinkler(from, to);
+    }
+
+    private void moveSprinklerRight(Coord from, int count) {
+        Coord to = new Coord(from.column + count, from.row);
+        moveSprinkler(from, to);
+    }
+
 }
--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -1,89 +1,172 @@
 package com.solutions.codility;
 
+import javafx.util.Pair;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
+    @Test
+    public void testZeroMoves() {
+        assertEquals(0, new Solution().solution(new int[]{1, 2, 3, 4, 5}, new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void testZeroMoves100000() {
+        final int size = 100000;
+        int[] X = new int[size];
+        int[] Y = new int[size];
+        int sprinklersCount = 0;
+        for (int x = 0; x < size; x++) {
+            X[sprinklersCount] = x + 1;
+            Y[sprinklersCount] = x + 1;
+            sprinklersCount++;
+        }
+        assertEquals(0, new Solution().solution(X, Y));
+    }
 
     @Test
     public void testAsDescribedInTask1() {
-        assertEquals(5, new Solution().solution(new int[]{1, 3, 6, 4, 1, 2}));
+        assertEquals(5, new Solution().solution(new int[]{1, 2, 2, 3, 4}, new int[]{1, 1, 4, 5, 4}));
     }
 
     @Test
     public void testAsDescribedInTask2() {
-        assertEquals(4, new Solution().solution(new int[]{1, 2, 3}));
+        assertEquals(6, new Solution().solution(new int[]{1, 1, 1, 1}, new int[]{1, 2, 3, 4}));
     }
+
+    @Test
+    public void testColumnsOnlyRightToLeft() {
+        assertEquals(6, new Solution().solution(new int[]{4, 4, 4, 4}, new int[]{1, 2, 3, 4}));
+    }
+
+    @Test
+    public void testColumnsOnlyBottomToTop() {
+        assertEquals(6, new Solution().solution(new int[]{1, 2, 3, 4}, new int[]{4, 4, 4, 4}));
+    }
+
+
+    @Test
+    public void testColumnsCenter() {
+        assertEquals(6, new Solution().solution(new int[]{3, 3, 3, 3, 3}, new int[]{1, 2, 3, 4, 5}));
+    }
+
 
     @Test
     public void testAsDescribedInTask3() {
-        assertEquals(1, new Solution().solution(new int[]{-1, -3}));
+        assertEquals(5, new Solution().solution(new int[]{1, 2, 2, 3, 4}, new int[]{1, 1, 4, 5, 4}));
     }
 
     @Test
-    public void testOnBiggerContinuousArray() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+    public void testPerformanceOnRandomField100() {
+        final int size = 100;
+        int[] X = new int[size];
+        int[] Y = new int[size];
+        Set<Pair<Integer, Integer>> sprinklers = new HashSet<>();
+        Random generator = new Random(42);
+        int sprinklersCount = 0;
+        while (sprinklersCount < size) {
+            Pair<Integer, Integer> coord = new Pair<>(generator.nextInt(size) + 1, generator.nextInt(size) + 1);
+            if (sprinklers.add(coord)) {
+                X[sprinklersCount] = coord.getKey();
+                Y[sprinklersCount] = coord.getValue();
+                sprinklersCount++;
+            }
         }
-        assertEquals(size + 1, new Solution().solution(A));
+        assertEquals(549, new Solution().solution(X, Y));
     }
 
     @Test
-    public void testOnBiggerLastItem() {
+    public void testPerformanceOnRandomField100000() {
         final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+        int[] X = new int[size];
+        int[] Y = new int[size];
+        Set<Pair<Integer, Integer>> sprinklers = new HashSet<>();
+        Random generator = new Random(42);
+        int sprinklersCount = 0;
+        while (sprinklersCount < size) {
+            Pair<Integer, Integer> coord = new Pair<>(generator.nextInt(size) + 1, generator.nextInt(size) + 1);
+            if (sprinklers.add(coord)) {
+                X[sprinklersCount] = coord.getKey();
+                Y[sprinklersCount] = coord.getValue();
+                sprinklersCount++;
+            }
         }
-
-        int gapIndex = 2;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
+        assertEquals(29356034, new Solution().solution(X, Y));
     }
 
     @Test
-    public void testOnBiggerArrayAlmostLast() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+    public void testPerformanceAllInOneSpot2() {
+        final int spotSize = 2;
+        final int size = spotSize * spotSize;
+        int[] X = new int[size];
+        int[] Y = new int[size];
+        int sprinklersCount = 0;
+        for (int x = 0; x < spotSize; x++) {
+            for (int y = 0; y < spotSize; y++) {
+                X[sprinklersCount] = x + 1;
+                Y[sprinklersCount] = y + 1;
+                sprinklersCount++;
+            }
         }
-        int gapIndex = size / 4;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
+        assertEquals(8, new Solution().solution(X, Y));
+    }
+
+
+    @Test
+    public void testPerformanceAllInOneSpot3() {
+        final int spotSize = 3;
+        final int size = spotSize * spotSize;
+        int[] X = new int[size];
+        int[] Y = new int[size];
+        int sprinklersCount = 0;
+        for (int x = 0; x < spotSize; x++) {
+            for (int y = 0; y < spotSize; y++) {
+                X[sprinklersCount] = x + 1;
+                Y[sprinklersCount] = y + 1;
+                sprinklersCount++;
+            }
+        }
+        assertEquals(54, new Solution().solution(X, Y));
     }
 
     @Test
-    public void testOnBiggerArrayHalfWay() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+    public void testPerformanceAllInOppositeSpot3() {
+        final int spotSize = 3;
+        final int size = spotSize * spotSize;
+        int[] X = new int[size];
+        int[] Y = new int[size];
+        int sprinklersCount = 0;
+        for (int x = size-spotSize; x < size; x++) {
+            for (int y = size-spotSize; y < size; y++) {
+                X[sprinklersCount] = x + 1;
+                Y[sprinklersCount] = y + 1;
+                sprinklersCount++;
+            }
         }
-
-        int gapIndex = size / 2;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-
+        assertEquals(54, new Solution().solution(X, Y));
     }
 
     @Test
-    public void testOnBiggerArrayInBeginning() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+    public void testPerformanceAllInOppositeSpot2() {
+        final int spotSize = 2;
+        final int size = spotSize * spotSize;
+        int[] X = new int[size];
+        int[] Y = new int[size];
+        int sprinklersCount = 0;
+        for (int x = size-spotSize; x < size; x++) {
+            for (int y = size-spotSize; y < size; y++) {
+                X[sprinklersCount] = x + 1;
+                Y[sprinklersCount] = y + 1;
+                sprinklersCount++;
+            }
         }
-
-        int gapIndex = 3 * size / 4;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
+        assertEquals(8, new Solution().solution(X, Y));
     }
+
+
 }
