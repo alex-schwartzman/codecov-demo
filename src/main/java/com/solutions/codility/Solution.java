05fde3f77304860ee13153f43c8ab9449001b1a3
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-11 23:57:27 +0200

    attempted to build better hint map for solution, but it only eats up heap

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,8 +1,13 @@
 package com.solutions.codility;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Solution {
+
+    Deque<Turtle> allUnknownTurtlesPending = new LinkedList<>();
 
     static class Result {
         public ArrayList<Integer> path = new ArrayList<>();
@@ -29,9 +34,58 @@ public class Solution {
         }
     }
 
+    class Turtle {
+
+        int row;
+        int column;
+
+        public Turtle(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        boolean step() {
+            byte direction = getKnownDirection();
+            if (direction == RIGHT) {
+                column++;
+                return true;
+            }
+            if (direction == DOWN) {
+                row++;
+                return true;
+            }
+            return false;
+        }
+
+        public boolean isValid() {
+            return !(column >= knownDirectionsMap[0].length || row >= knownDirectionsMap.length || isInRightBottomCorner());
+        }
+
+        private boolean isInRightBottomCorner() {
+            return column == knownDirectionsMap[0].length - 1 && row == knownDirectionsMap.length - 1;
+        }
+
+        public byte getKnownDirection() {
+            if (column >= knownDirectionsMap[0].length || row >= knownDirectionsMap.length) {
+                return UNDEFINED;
+            }
+            return knownDirectionsMap[row][column];
+        }
+
+        public int getValue(int[][] a) {
+            return a[row][column];
+        }
+
+        public boolean isEqual(Turtle other) {
+            return this.column == other.column && this.row == other.row;
+        }
+    }
+
     byte[][] knownDirectionsMap;
     static final byte DOWN = 1;
     static final byte RIGHT = 2;
+    static final byte UNDEFINED = 0;
+    static final byte MARKEDUNKNOWN = 3;
 
     public String solution(int[][] A) {
         final int width = A[0].length;
@@ -52,8 +106,51 @@ public class Solution {
                 }
             }
         }
+
+        markAllUnknowns(0, 0);
+
+
+        while (unknownTurtles.size() > 0) {
+            Turtle currentTurtle = unknownTurtles.remove(unknownTurtles.size() - 1);
+            Turtle rightTurtle = new Turtle(currentTurtle.row, currentTurtle.column + 1);
+            Turtle downTurtle = new Turtle(currentTurtle.row + 1, currentTurtle.column);
+            while (rightTurtle.step() && downTurtle.step()) {
+                int rightTurtleValue = rightTurtle.getValue(A);
+                int downTurtleValue = downTurtle.getValue(A);
+                if (rightTurtleValue > downTurtleValue || rightTurtle.isEqual(downTurtle)) {
+                    knownDirectionsMap[currentTurtle.row][currentTurtle.column] = RIGHT;
+                    break;
+                } else if (downTurtleValue > rightTurtleValue) {
+                    knownDirectionsMap[currentTurtle.row][currentTurtle.column] = DOWN;
+                    break;
+                }
+            }
+        }
+
         return toString(solution(A, 0, 0).path);
     }
+
+    private void markAllUnknowns(int row, int column) {
+        Turtle t = new Turtle(row, column);
+        allUnknownTurtlesPending.addLast(t);
+
+        while(!allUnknownTurtlesPending.isEmpty()){
+            t = allUnknownTurtlesPending.removeFirst();
+            if (!t.isValid()) {
+                return;
+            }
+            while (t.step()) {
+            }
+            if (t.isValid()) {
+                knownDirectionsMap[t.row][t.column] = MARKEDUNKNOWN;
+                unknownTurtles.add(t);
+                allUnknownTurtlesPending.addLast(new Turtle(t.row + 1, t.column));
+                allUnknownTurtlesPending.addLast(new Turtle(t.row, t.column + 1));
+            }
+        }
+    }
+
+    ArrayList<Turtle> unknownTurtles = new ArrayList<>();
 
     private String toString(ArrayList<Integer> array) {
         StringBuilder b = new StringBuilder();
