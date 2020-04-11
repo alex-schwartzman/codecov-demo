21f21f6ab84d4f23cdf181608bf6f67b507e709b
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-11 19:02:05 +0200

    implemented using recursion. complexity O(N!)

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,18 +1,57 @@
 package com.solutions.codility;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
 
 public class Solution {
-    public int solution(int[] A) {
-        AtomicInteger result = new AtomicInteger(1);
 
-        //Fancy stream-only way, potentially buggy, and anti-functional (in functional programming we avoid side effects, whereas here we utilize a side-effect of rewriting result)
-        IntStream.of(A).filter(x -> x > 0).sorted().distinct().forEachOrdered(i -> {
-            if (i == result.get()) {
-                result.set(i + 1);
-            }
-        });
-        return result.get();
+    static class Result {
+        public ArrayList<Integer> path = new ArrayList<>();
+        public int sum = 0;
+
+        public Result append(int i) {
+            path.add(i);
+            sum += i;
+            return this;
+        }
     }
+
+    public String solution(int[][] A) {
+        return toString(solution(A, 0, 0).path);
+    }
+
+    private String toString(ArrayList<Integer> array) {
+        StringBuilder b = new StringBuilder();
+        for (int i : array) {
+            b.append(i);
+        }
+        return b.reverse().toString();
+    }
+
+    private Result solution(int[][] A, int row, int column) {
+        Result rightOption = null;
+        Result downOption = null;
+        if (A.length - 1 > row) {
+            //excercise down option - next row
+            downOption = solution(A, row + 1, column);
+        }
+        if (A[row].length - 1 > column) {
+            //excercise right option - next item in the same row (or we say next column)
+            rightOption = solution(A, row, column + 1);
+        }
+
+        Result optimalResult = null;
+
+        if (rightOption == null && downOption == null) {
+            optimalResult = new Result();
+        } else if (rightOption == null) {
+            optimalResult = downOption;
+        } else if (downOption == null) {
+            optimalResult = rightOption;
+        } else {
+            optimalResult = rightOption.sum > downOption.sum ? rightOption : downOption;
+        }
+        return optimalResult.append(A[row][column]);
+    }
+
+
 }
--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -1,89 +1,74 @@
 package com.solutions.codility;
 
+import javafx.util.Pair;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
 
     @Test
+    public void oneSizedArray() {
+        assertEquals("6", new Solution().solution(new int[][]{{6}}));
+    }
+
+    @Test
+    public void oneDimensionalArray() {
+        assertEquals("123456789", new Solution().solution(new int[][]{{1, 2, 3, 4, 5, 6, 7, 8, 9}}));
+    }
+
+    @Test
+    public void twoRowsArray() {
+        assertEquals("1234567899", new Solution().solution(new int[][]{{1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}}));
+    }
+
+    @Test
     public void testAsDescribedInTask1() {
-        assertEquals(5, new Solution().solution(new int[]{1, 3, 6, 4, 1, 2}));
+        assertEquals("997952", new Solution().solution(new int[][]{{9, 9, 7}, {9, 7, 2}, {6, 9, 5}, {9, 1, 2}}));
     }
 
     @Test
-    public void testAsDescribedInTask2() {
-        assertEquals(4, new Solution().solution(new int[]{1, 2, 3}));
-    }
-
-    @Test
-    public void testAsDescribedInTask3() {
-        assertEquals(1, new Solution().solution(new int[]{-1, -3}));
-    }
-
-    @Test
-    public void testOnBiggerContinuousArray() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+    public void testPerformanceOnRandomField100x100() {
+        final int size = 100;
+        int[][] A = new int[size][size];
+        Random generator = new Random(42);
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                A[x][y] = generator.nextInt(9) + 1;
+            }
         }
-        assertEquals(size + 1, new Solution().solution(A));
+        assertEquals("Hello World", new Solution().solution(A));
     }
 
     @Test
-    public void testOnBiggerLastItem() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+    public void testPerformanceOnRandomField10x10() {
+        final int size = 10;
+        int[][] A = new int[size][size];
+        Random generator = new Random(42);
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                A[x][y] = generator.nextInt(9) + 1;
+            }
         }
-
-        int gapIndex = 2;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
+        assertEquals("9497976869986774795", new Solution().solution(A));
     }
 
     @Test
-    public void testOnBiggerArrayAlmostLast() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+    public void testPerformanceOnRandomField30x30() {
+        final int size = 30;
+        int[][] A = new int[size][size];
+        Random generator = new Random(42);
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                A[x][y] = generator.nextInt(9) + 1;
+            }
         }
-        int gapIndex = size / 4;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
+        assertEquals("Hello World", new Solution().solution(A));
     }
 
-    @Test
-    public void testOnBiggerArrayHalfWay() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
 
-        int gapIndex = size / 2;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-
-    }
-
-    @Test
-    public void testOnBiggerArrayInBeginning() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-
-        int gapIndex = 3 * size / 4;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-    }
 }
