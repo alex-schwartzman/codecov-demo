031d450cd7f16dcb787eb2c7af3a1010c6d06a8b
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-12 22:52:15 +0200

    added more tests, for performance improvement

--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -1,16 +1,42 @@
 package com.solutions.codility;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
 
+    static int[] village1000LinedUp;
+    static int[] village1000OddEven;
+
+    @BeforeClass
+    public static void warmUpAllVillages() {
+        //1000 villages in a row
+        final int size = 1000;
+        village1000LinedUp = new int[size];
+        for (int i = 0; i < size; i++) {
+            village1000LinedUp[i] = i + 1;
+        }
+        village1000LinedUp[size - 1] = size - 1;
+
+        //1000 villages first-all-odd, then-all-even
+        village1000OddEven = new int[size];
+        for (int i = 0; i < size - 2; i++) {
+            village1000OddEven[i] = i + 2;
+        }
+        village1000OddEven[size - 1] = size - 1;
+        village1000OddEven[size - 2] = size - 1;
+
+
+    }
+
     @Test
     public void testSingleVillage() {
         assertEquals(1, new Solution().solution(new int[]{0}));
     }
-
 
     @Test
     public void testAsDescribedInTask1() {
@@ -30,6 +56,16 @@ public class SolutionTest {
     @Test
     public void testThreeVillages021() {
         assertEquals(5, new Solution().solution(new int[]{2, 2, 2}));
+    }
+
+    @Test
+    public void test1000VillagesInARow() {
+        assertEquals(500500, new Solution().solution(village1000LinedUp));
+    }
+
+    @Test
+    public void test1000VillagesOddEven() {
+        assertEquals(1999, new Solution().solution(village1000OddEven));
     }
 
 }
