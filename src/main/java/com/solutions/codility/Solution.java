9610c616c8e6091b92a514b4f28d1eefeacd9414
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-13 16:32:58 +0200

    dumb and short solution. Correct answers, too slow.

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -5,42 +5,11 @@ import java.util.ArrayList;
 public class Solution {
 
     static ArrayList<Integer>[] edges;
-    static byte[][] answers;
-    static byte NO = 2;
-    static byte INTERCONNECTED = 1;
-    static byte UNDEFINED = 0;
     int result = 0;
-
-    void setAnswer(int from, int to, byte answer) {
-        if (from > to) {
-            //swap
-            final int tmp = to;
-            to = from;
-            from = tmp;
-        }
-        if (answers[from][to] != answer) {
-            answers[from][to] = answer;
-            if (answer == INTERCONNECTED) {
-//                System.out.printf(" %d:%d %d\n", from, to, answer);
-                result++;
-            }
-        }
-    }
-
-    byte getAnswer(int from, int to) {
-        if (from > to) {
-            //swap
-            final int tmp = to;
-            to = from;
-            from = tmp;
-        }
-        return answers[from][to];
-    }
 
     public int solution(int[] A) {
         //noinspection unchecked
         edges = new ArrayList[A.length];
-        answers = new byte[A.length][A.length];
 
         for (int i = 0; i < A.length; i++) {
             edges[i] = new ArrayList<>();
@@ -56,34 +25,27 @@ public class Solution {
             }
         }
 
-        setAnswer(A.length - 1, A.length - 1, INTERCONNECTED);
-        for (int from = 0; from < A.length - 1; from++) {
-            setAnswer(from, from, INTERCONNECTED);
-            for (int to = from + 1; to < A.length; to++) {
+        for (int from = 0; from < A.length; from++) {
+            for (int to = from; to < A.length; to++) {
                 if (isConnectedGraph(from, to)) {
-                    setAnswer(from, to, INTERCONNECTED);
+                    result++;
                 }
             }
         }
         return result;
     }
 
-    //looks awesome, but is inherently incorrect
     private boolean isConnectedGraph(int from, int to) {
-        byte[] results = new byte[to - from + 1];
+        int connectionsCount = 0;
+
         for (int vertex = from; vertex <= to; vertex++) {
             for (Integer v : edges[vertex]) {
                 if (from <= v && v <= to) {
-                    results[v - from] = 1;
+                    connectionsCount++;
                 }
             }
         }
 
-        for (byte b : results) {
-            if (b != 1) {
-                return false;
-            }
-        }
-        return true;
+        return connectionsCount == (to - from) * 2;
     }
 }
