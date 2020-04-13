88f66a05ecfa2fc0078fd073cf4591d9629e5e28
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-13 21:29:27 +0200

    return back to readable solution, 20% less efficient, but yielding the same 63% on codility

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,53 +1,44 @@
 package com.solutions.codility;
 
+import java.util.ArrayList;
+
 public class Solution {
+
+    static ArrayList<Integer>[] edges;
     int result = 0;
 
     public int solution(int[] A) {
-        int[] edgeCounts = new int[A.length];
-        int[][] hardEdges = new int[A.length][A.length];
+        //noinspection unchecked
+        edges = new ArrayList[A.length];
+
         for (int i = 0; i < A.length; i++) {
+            edges[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            //noinspection UnnecessaryLocalVariable
             int from = i;
             int to = A[i];
             if (from != to) {
-                hardEdges[from][edgeCounts[from]] = to;
-                hardEdges[to][edgeCounts[to]] = from;
-                edgeCounts[from]++;
-                edgeCounts[to]++;
+                edges[from].add(to);
+                edges[to].add(from);
             }
         }
 
-        int rowEnd = A.length;
         for (int from = 0; from < A.length; from++) {
             int[] edgeCounters = new int[A.length];
-            for (int to = from; to < rowEnd; to++) {
-                for (int j = 0; j < edgeCounts[to]; j++) {
-                    edgeCounters[hardEdges[to][j]]++;
+            for (int to = from; to < A.length; to++) {
+                for (Integer v : edges[to]) {
+                    edgeCounters[v]++;
                 }
                 int connectionsCount = 0;
                 for (int i = from; i <= to; i++) {
                     connectionsCount += edgeCounters[i];
                 }
-                boolean connectedOK = (connectionsCount == (to - from) * 2);
-                if (connectedOK) {
+                if (connectionsCount == (to - from) * 2) {
                     result++;
                 }
             }
-            //backwards
-            for (int leftSideSliding = from; leftSideSliding < rowEnd; leftSideSliding++) {
-                for (int j = 0; j < edgeCounts[leftSideSliding]; j++) {
-                    edgeCounters[hardEdges[leftSideSliding][j]]--;
-                }
-                int connectionsCount = 0;
-                for (int i = leftSideSliding + 1; i < rowEnd; i++) {
-                    connectionsCount += edgeCounters[i];
-                }
-                boolean connectedOK = (connectionsCount == (rowEnd - leftSideSliding - 2) * 2);
-                if (connectedOK) {
-                    result++;
-                }
-            }
-            rowEnd--;
         }
         return result;
     }
