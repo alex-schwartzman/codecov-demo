b490ead5191f00d51b2dbf6fb9f5fb175d3a587e
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-13 21:14:54 +0200

    got rid of java.util.ArrrayList, just to measure if it gets any better It does not. Now this big page with edges does not squeeze into cache, so it is even slower, despite that couple of layers of indirections were removed.

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,41 +1,19 @@
 package com.solutions.codility;
 
-import java.util.ArrayList;
-
 public class Solution {
-
-    static ArrayList<Integer>[] edges;
     int result = 0;
 
     public int solution(int[] A) {
-        //noinspection unchecked
-        edges = new ArrayList[A.length];
-
+        int[] edgeCounts = new int[A.length];
+        int[][] hardEdges = new int[A.length][A.length];
         for (int i = 0; i < A.length; i++) {
-            edges[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < A.length; i++) {
-            //noinspection UnnecessaryLocalVariable
             int from = i;
             int to = A[i];
             if (from != to) {
-                edges[from].add(to);
-                edges[to].add(from);
-            }
-        }
-
-        int maxLength = 0;
-        for (ArrayList<Integer> l : edges) {
-            maxLength = Math.max(maxLength, l.size());
-        }
-
-        int[] edgeCounts = new int[A.length];
-        int[][] hardEdges = new int[A.length][maxLength];
-        for (int i = 0; i < A.length; i++) {
-            edgeCounts[i] = edges[i].size();
-            for (int j = 0; j < edgeCounts[i]; j++) {
-                hardEdges[i][j] = edges[i].get(j);
+                hardEdges[from][edgeCounts[from]] = to;
+                hardEdges[to][edgeCounts[to]] = from;
+                edgeCounts[from]++;
+                edgeCounts[to]++;
             }
         }
 
