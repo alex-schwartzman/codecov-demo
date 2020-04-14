c7e7cc6610c672236039c94e56187edf66d97fff
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-14 12:31:32 +0200

    https://app.codility.com/programmers/task/replacing_books/ Given a list of integers, return the maximum number of consecutive integers equal to each other after replacing at most K of them.

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,18 +1,7 @@
 package com.solutions.codility;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
-
 public class Solution {
-    public int solution(int[] A) {
-        AtomicInteger result = new AtomicInteger(1);
-
-        //Fancy stream-only way, potentially buggy, and anti-functional (in functional programming we avoid side effects, whereas here we utilize a side-effect of rewriting result)
-        IntStream.of(A).filter(x -> x > 0).sorted().distinct().forEachOrdered(i -> {
-            if (i == result.get()) {
-                result.set(i + 1);
-            }
-        });
-        return result.get();
+    public int solution(int[] A, int K) {
+        return 42;
     }
 }
--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -8,82 +8,27 @@ public class SolutionTest {
 
     @Test
     public void testAsDescribedInTask1() {
-        assertEquals(5, new Solution().solution(new int[]{1, 3, 6, 4, 1, 2}));
+        assertEquals(2, new Solution().solution(new int[]{1, 1, 3, 4, 3, 3, 4}, 2));
     }
 
     @Test
     public void testAsDescribedInTask2() {
-        assertEquals(4, new Solution().solution(new int[]{1, 2, 3}));
+        assertEquals(2, new Solution().solution(new int[]{4, 5, 5, 4, 2, 2, 4}, 0));
     }
 
     @Test
     public void testAsDescribedInTask3() {
-        assertEquals(1, new Solution().solution(new int[]{-1, -3}));
+        assertEquals(4, new Solution().solution(new int[]{1, 3, 3, 2}, 2));
     }
 
     @Test
     public void testOnBiggerContinuousArray() {
-        final int size = 100000;
+        final int size = 100;
         int[] A = new int[size];
         for (int i = 0; i < A.length; i++) {
             A[i] = A.length - i;
         }
-        assertEquals(size + 1, new Solution().solution(A));
+        assertEquals(6, new Solution().solution(A, 5));
     }
 
-    @Test
-    public void testOnBiggerLastItem() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-
-        int gapIndex = 2;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-    }
-
-    @Test
-    public void testOnBiggerArrayAlmostLast() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-        int gapIndex = size / 4;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-    }
-
-    @Test
-    public void testOnBiggerArrayHalfWay() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-
-        int gapIndex = size / 2;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-
-    }
-
-    @Test
-    public void testOnBiggerArrayInBeginning() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-
-        int gapIndex = 3 * size / 4;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-    }
 }
