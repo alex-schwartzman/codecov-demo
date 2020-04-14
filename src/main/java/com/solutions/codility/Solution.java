9a3d703f2ecbf4c6b52e166f93154f280cf82ebd
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-14 21:15:17 +0200

    53% naive solution, and big tests implemented. Now need to refactor for better performance

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,7 +1,31 @@
 package com.solutions.codility;
 
+import java.util.TreeSet;
+
+//https://app.codility.com/programmers/task/replacing_books/
+//Given a list of integers, return the maximum number of consecutive integers equal to each other after replacing at most K of them.
 public class Solution {
     public int solution(int[] A, int K) {
-        return 42;
+        TreeSet<Integer> uniqueBooks = new TreeSet<>();
+        for (int a : A) {
+            uniqueBooks.add(a);
+        }
+        int result = 0;
+        for (int currentBoook : uniqueBooks) {
+            for (int from = 0; from < A.length; from++) {
+                int kLocal = K;
+                int counter = 0;
+                for (int to = from; to < A.length && kLocal >= 0; to++) {
+                    if (A[to] != currentBoook) {
+                        kLocal--;
+                    }
+                    if (kLocal >= 0) {
+                        counter++;
+                    }
+                }
+                result = Math.max(result, counter);
+            }
+        }
+        return result;
     }
 }
--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -1,6 +1,9 @@
 package com.solutions.codility;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,7 +11,7 @@ public class SolutionTest {
 
     @Test
     public void testAsDescribedInTask1() {
-        assertEquals(2, new Solution().solution(new int[]{1, 1, 3, 4, 3, 3, 4}, 2));
+        assertEquals(5, new Solution().solution(new int[]{1, 1, 3, 4, 3, 3, 4}, 2));
     }
 
     @Test
@@ -24,6 +27,125 @@ public class SolutionTest {
     @Test
     public void testOnBiggerContinuousArray() {
         final int size = 100;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - i;
+        }
+        assertEquals(6, new Solution().solution(A, 5));
+    }
+
+    @Test
+    public void testOnBiggerRandomArray() {
+        final int size = 100;
+        int[] A = new int[size];
+        Random generator = new Random(42);
+        for (int i = 0; i < A.length; i++) {
+            A[i] = generator.nextInt(size / 2) + 1;
+        }
+        assertEquals(8, new Solution().solution(A, 5));
+    }
+
+    @Test
+    public void testOn1kRandomArray() {
+        final int size = 1000;
+        int[] A = new int[size];
+        Random generator = new Random(42);
+        for (int i = 0; i < A.length; i++) {
+            A[i] = generator.nextInt(size / 2) + 1;
+        }
+        assertEquals(7, new Solution().solution(A, 5));
+    }
+
+    @Test
+    public void testOn10kRandomArray() {
+        final int size = 10000;
+        int[] A = new int[size];
+        Random generator = new Random(42);
+        for (int i = 0; i < A.length; i++) {
+            A[i] = generator.nextInt(size / 2) + 1;
+        }
+        assertEquals(53, new Solution().solution(A, 50));
+    }
+
+    @Test
+    @Ignore //for performance reasons :)
+    public void testOn100kRandomArray() {
+        final int size = 100000;
+        int[] A = new int[size];
+        Random generator = new Random(42);
+        for (int i = 0; i < A.length; i++) {
+            A[i] = generator.nextInt(size / 2) + 1;
+        }
+        assertEquals(7, new Solution().solution(A, 500));
+    }
+
+    @Test
+    public void testOnBiggerOddEvenArray() {
+        final int size = 100;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = i % 2 == 0 ? 1 : 2;
+        }
+        assertEquals(95, new Solution().solution(A, size / 2 - 3));
+    }
+
+    @Test
+    public void testOn1kOddEvenArray() {
+        final int size = 10000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = i % 2 == 0 ? 1 : 2;
+        }
+        assertEquals(9995, new Solution().solution(A, size / 2 - 3));
+    }
+
+    @Test
+    public void testOn10kOddEvenArray() {
+        final int size = 100000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = i % 2 == 0 ? 1 : 2;
+        }
+        assertEquals(99995, new Solution().solution(A, size / 2 - 3));
+    }
+
+
+    @Test
+    @Ignore //for performance reasons :)
+    public void testOn30kOddEvenArray() {
+        final int size = 300000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = i % 2 == 0 ? 1 : 2;
+        }
+        assertEquals(99995, new Solution().solution(A, size / 2 - 3));
+    }
+
+    @Test
+    public void testOn1kContinuousArray() {
+        final int size = 1000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - i;
+        }
+        assertEquals(6, new Solution().solution(A, 5));
+    }
+
+    @Test
+    public void testOn10kContinuousArray() {
+        final int size = 10000;
+        int[] A = new int[size];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A.length - i;
+        }
+        assertEquals(6, new Solution().solution(A, 5));
+    }
+
+
+    @Test
+    @Ignore //for performance reasons :)
+    public void testOn100kContinuousArray() {
+        final int size = 100000;
         int[] A = new int[size];
         for (int i = 0; i < A.length; i++) {
             A[i] = A.length - i;
