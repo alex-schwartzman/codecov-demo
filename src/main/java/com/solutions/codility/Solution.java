933412a9b92b8ca7297a9987d9ff44583734942a
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-16 11:11:02 +0200

    73% performance tests OK, 2 wrong answers

--- src/main/java/com/solutions/codility/Solution.java
+++ src/main/java/com/solutions/codility/Solution.java
@@ -1,18 +1,50 @@
 package com.solutions.codility;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
-
+//https://app.codility.com/programmers/task/flipping_matrix/
+//idea is that we go column-by-column and match only the rows which stayed alive after previous iterations. That's thanks to reversibility of XOR 0xFF
 public class Solution {
-    public int solution(int[] A) {
-        AtomicInteger result = new AtomicInteger(1);
+    public int solution(int[][] A) {
+        if (A.length <= 1) {
+            return A.length;
+        }
 
-        //Fancy stream-only way, potentially buggy, and anti-functional (in functional programming we avoid side effects, whereas here we utilize a side-effect of rewriting result)
-        IntStream.of(A).filter(x -> x > 0).sorted().distinct().forEachOrdered(i -> {
-            if (i == result.get()) {
-                result.set(i + 1);
+        //this is column with answers, full of zeroes. From now on zero - is the right answer which survived xoring next column
+        int[] answers = new int[A.length];
+
+        for (int column = 1; column < A[0].length; column++) {
+            int plainMatchCounter = 0;
+            int reverseMatchCounter = 0;
+            for (int row = 0; row < A.length; row++) {
+                if (answers[row] == 0) {
+                    if (A[row][column - 1] == A[row][column]) {
+                        plainMatchCounter++;
+                    } else {
+                        reverseMatchCounter++;
+                    }
+                }
             }
-        });
-        return result.get();
+
+            for (int row = 0; row < A.length; row++) {
+                int leftCellValue = A[row][column - 1];
+                int rightCellValue = A[row][column];
+                if (answers[row] == 0) {
+                    if (reverseMatchCounter > plainMatchCounter) {
+                        rightCellValue = 1 - rightCellValue;
+                    }
+                    if (answers[row] == 0 && rightCellValue != leftCellValue) {
+                        answers[row] = 1; //this row is done and will never be compared anymore, since it fails to form a row up to column number int "column"
+                    }
+                }
+            }
+        }
+
+        int result = 0;
+        for (int a : answers) {
+            if (a == 0) {
+                result++;
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -8,82 +8,25 @@ public class SolutionTest {
 
     @Test
     public void testAsDescribedInTask1() {
-        assertEquals(5, new Solution().solution(new int[]{1, 3, 6, 4, 1, 2}));
+        assertEquals(2, new Solution().solution(new int[][]{{0, 0, 0, 0}, {0, 1, 0, 0}, {1, 0, 1, 1}}));
     }
 
     @Test
     public void testAsDescribedInTask2() {
-        assertEquals(4, new Solution().solution(new int[]{1, 2, 3}));
+        assertEquals(4, new Solution().solution(new int[][]{{0, 1, 0, 1}, {1, 0, 1, 0}, {0, 1, 0, 1}, {1, 0, 1, 0}}));
     }
 
     @Test
-    public void testAsDescribedInTask3() {
-        assertEquals(1, new Solution().solution(new int[]{-1, -3}));
-    }
-
-    @Test
-    public void testOnBiggerContinuousArray() {
+    public void testOneLiner() {
         final int size = 100000;
         int[] A = new int[size];
+        int[] B = new int[size];
         for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
+            A[i] = i % 2;
+            B[i] = 1 - i % 2;
         }
-        assertEquals(size + 1, new Solution().solution(A));
+        assertEquals(2, new Solution().solution(new int[][]{A, B}));
+        assertEquals(1, new Solution().solution(new int[][]{A}));
     }
 
-    @Test
-    public void testOnBiggerLastItem() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-
-        int gapIndex = 2;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-    }
-
-    @Test
-    public void testOnBiggerArrayAlmostLast() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-        int gapIndex = size / 4;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-    }
-
-    @Test
-    public void testOnBiggerArrayHalfWay() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-
-        int gapIndex = size / 2;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-
-    }
-
-    @Test
-    public void testOnBiggerArrayInBeginning() {
-        final int size = 100000;
-        int[] A = new int[size];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A.length - i;
-        }
-
-        int gapIndex = 3 * size / 4;
-        int expectedResult = A[gapIndex];
-        A[gapIndex] = -1;
-        assertEquals(expectedResult, new Solution().solution(A));
-    }
 }
