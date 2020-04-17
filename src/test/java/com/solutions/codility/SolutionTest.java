cd66c5395ec3315f8ea9ce5617112ab4b62eaef7
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-04-17 18:28:13 +0200

    71% added performance test

--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -1,6 +1,9 @@
 package com.solutions.codility;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,5 +27,33 @@ public class SolutionTest {
     @Test
     public void testAsDescribedInTask4() {
         assertEquals(-1, new Solution().solution("aaaa", 2));
+    }
+
+    @Test
+    public void testOnLongArray10000() {
+        int size = 10000;
+        char[] A = new char[size];
+        Random generator = new Random(42);
+        for (int i = 0; i < size; i++) {
+            A[i] = (char) ('a' + (char) (generator.nextInt(25)));
+        }
+        final String input = new String(A);
+        assertEquals(9906, new Solution().solution(input, 24));
+        assertEquals(9972, new Solution().solution(input, 14));
+        assertEquals(9995, new Solution().solution(input, 5));
+    }
+
+    @Test
+    @Ignore //takes 10s on my machine
+    public void testOnLongArray100000() {
+        int size = 50000;
+        char[] A = new char[size];
+        Random generator = new Random(42);
+        for (int i = 0; i < size; i++) {
+            A[i] = (char) ('a' + (char) (generator.nextInt(25)));
+        }
+        final String input = new String(A);
+        assertEquals(49888, new Solution().solution(input, 24));
+        assertEquals(49973, new Solution().solution(input, 14));
     }
 }
