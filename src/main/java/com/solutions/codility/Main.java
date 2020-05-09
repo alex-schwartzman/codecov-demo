d9bda53c33343a42db2d020f2267659a25dc48c9
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-05-09 20:45:49 +0200

    A, B, C, D, F solved

--- src/main/java/com/solutions/codility/Main.java
+++ src/main/java/com/solutions/codility/Main.java
@@ -1,8 +1,213 @@
 package com.solutions.codility;
 
-public class Main {
+import java.util.ArrayDeque;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        new Solution().solution(new int[]{1, 2, 3, 4, 5});
-    }
-}
+//D - Alice, Bob and Candies https://codeforces.com/contest/1352/problem/D https://codeforces.com/contest/1352/submission/79586038
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int count = in.nextInt();
+//        for (int i = 0; i < count; i++) {
+//            int candieCount = in.nextInt();
+//            ArrayDeque<Integer> candiesRow = new ArrayDeque<>();
+//            while (candieCount-- > 0) {
+//                candiesRow.add(in.nextInt());
+//            }
+//
+//            System.out.println(solve(candiesRow));
+//        }
+//    }
+//
+//    public static String solve(ArrayDeque<Integer> candiesRow) {
+//        int turnCount = 0, aliceSize = 0, bobSize = 0, maxEatenInATurn = 0, currentlyEatenInATurn = 0;
+//        while (!candiesRow.isEmpty()) {
+//            if (currentlyEatenInATurn == 0) {
+//                turnCount++;
+//            }
+//            if (turnCount % 2 == 1) {
+//                //Alice moves
+//                final int nextCandie = candiesRow.removeFirst();
+//                aliceSize += nextCandie;
+//                currentlyEatenInATurn += nextCandie;
+//                if (currentlyEatenInATurn > maxEatenInATurn) {
+//                    //switch to Bob
+//                    maxEatenInATurn = currentlyEatenInATurn;
+//                    currentlyEatenInATurn = 0;
+//                }
+//            } else {
+//                //Bob moves
+//                final int nextCandie = candiesRow.removeLast();
+//                bobSize += nextCandie;
+//                currentlyEatenInATurn += nextCandie;
+//                if (currentlyEatenInATurn > maxEatenInATurn) {
+//                    //switch to Alice
+//                    maxEatenInATurn = currentlyEatenInATurn;
+//                    currentlyEatenInATurn = 0;
+//                }
+//            }
+//        }
+//        return String.format("%d %d %d", turnCount, aliceSize, bobSize);
+//    }
+//}
+
+//F - Binary String Reconstruction https://codeforces.com/contest/1352/problem/F https://codeforces.com/contest/1352/submission/79581910
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int count = in.nextInt();
+//        for (int i = 0; i < count; i++) {
+//            int n0 = in.nextInt();
+//            int n1 = in.nextInt();
+//            int n2 = in.nextInt();
+//            System.out.println(solve(n0, n1, n2));
+//        }
+//    }
+//
+//    public static String solve(int n0, int n1, int n2) {
+//        StringBuilder result = new StringBuilder();
+//        if (n1 == 0) {
+//            if (n0 != 0) {
+//                repetitiveAdd(result, "0", n0 + 1);
+//            } else if (n2 != 0) {
+//                repetitiveAdd(result, "1", n2 + 1);
+//            } else {
+//                return "Invalid Input";
+//            }
+//        } else {
+//            if (n1 % 2 == 0 && n1 > 0) {
+//                result.append('1');
+//                n1--;
+//            }
+//            //from now on, n1 is odd
+//            repetitiveAdd(result, "0", n0 + 1);
+//            repetitiveAdd(result, "10", n1 / 2);
+//            repetitiveAdd(result, "1", n2 + 1);
+//        }
+//        return result.toString();
+//    }
+//
+//    private static void repetitiveAdd(StringBuilder result, String value, int count) {
+//        while (count-- > 0) {
+//            result.append(value);
+//        }
+//    }
+//
+//}
+
+
+//C - K-th Not Divisible by n https://codeforces.com/contest/1352/problem/C https://codeforces.com/contest/1352/submission/79549060
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int count = in.nextInt();
+//        for (int i = 0; i < count; i++) {
+//            int n = in.nextInt();
+//            int k = in.nextInt();
+//            System.out.println(solve(n, k));
+//            ;
+//        }
+//    }
+//
+//    public static int solve(int n, int k) {
+//        int remainder = k;
+//        int start = 0;
+//        while (remainder >= n) {
+//            int skipPart = remainder / n;
+//            remainder = remainder % n + skipPart;
+//            start += skipPart * n;
+//        }
+//        return start + remainder;
+//    }
+//}
+
+//
+//    //B - Same Parity Summands https://codeforces.com/contest/1352/problem/B https://codeforces.com/contest/1352/submission/79525295
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int count = in.nextInt();
+//        for (int i = 0; i < count; i++) {
+//            int n = in.nextInt();
+//            int k = in.nextInt();
+//            LinkedList<String> results = solve(n, k);
+//            if (results != null) {
+//                System.out.println("YES");
+//                System.out.println(String.join(" ", results));
+//            } else {
+//                System.out.println("NO");
+//            }
+//        }
+//    }
+//
+//    public static LinkedList<String> solve(int n, int k) {
+//        LinkedList<String> results = new LinkedList<>();
+//
+//        final boolean nIsEven = n % 2 == 0;
+//        final boolean kIsEven = k % 2 == 0;
+//        if (nIsEven) {
+//            if (kIsEven) {
+//                if (n < k) {
+//                    return null;
+//                }
+//                while (k > 1) {
+//                    k--;
+//                    results.add("1");
+//                    n -= 1;
+//                }
+//                results.add(String.valueOf(n));
+//            } else {
+//                //k is odd - therefore we cannot afford odd results => should check multiplication by 2
+//                if (n < k * 2) {
+//                    return null;
+//                }
+//                while (k > 1) {
+//                    k--;
+//                    results.add("2");
+//                    n -= 2;
+//                }
+//                results.add(String.valueOf(n));
+//            }
+//        } else {
+//            if (kIsEven) {
+//                return null;
+//            } else {
+//                if (n < k) {
+//                    return null;
+//                }
+//                while (k > 1) {
+//                    k--;
+//                    results.add("1");
+//                    n -= 1;
+//                }
+//                results.add(String.valueOf(n));
+//            }
+//        }
+//
+//        return results;
+//    }
+//}
+//
+
+////A - Sum of Round Numbers https://codeforces.com/contest/1352/problem/A https://codeforces.com/contest/1352/submission/79494122
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int count = in.nextInt();
+//        for (int i = 0; i < count; i++) {
+//            int number = in.nextInt();
+//            LinkedList<String> results = new LinkedList<>();
+//            int multiplier = 1;
+//            while (number > 0) {
+//                int resultItem = number % 10;
+//                if (resultItem != 0) {
+//                    results.add(String.valueOf(resultItem * multiplier));
+//                }
+//                number /= 10;
+//                multiplier*=10;
+//            }
+//            System.out.println(results.size());
+//            System.out.println(String.join(" ", results));
+//        }
+//    }
--- src/test/java/com/solutions/codility/SolutionTest.java
+++ src/test/java/com/solutions/codility/SolutionTest.java
@@ -2,18 +2,125 @@ package com.solutions.codility;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 
 public class SolutionTest {
 
-    @Test
-    public void testAsDescribedInTask(){
-        assertEquals(42, new Solution().solution(new int[]{1,2,3,4}));
-    }
+//    //D - Alice, Bob and Candies https://codeforces.com/contest/1352/problem/D https://codeforces.com/contest/1352/submission/79586038
+//    @Test
+//    public void testMyTestNoLeadingZeros3() {
+//        assertEquals("0 0 0", Main.solve(new ArrayDeque<>()));
+//    }
+//
+//    @Test
+//    public void testAsDescribed1() {
+//        assertEquals("6 23 21", Main.solve(new ArrayDeque<Integer>(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5))));
+//    }
+//
+//    //1 2 3 4 5 6 7 8 9 10 11 12 13
+//    @Test
+//    public void testAsDescribed2() {
+//        assertEquals("1 1000 0", Main.solve(new ArrayDeque<Integer>(Arrays.asList(1000))));
+//    }
+//
+//    @Test
+//    public void testAsDescribed3() {
+//        assertEquals("6 45 46", Main.solve(new ArrayDeque<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13))));
+//    }
+//
 
-    @Test
-    public void testOnBiggerArrays(){
-        assertEquals(42, new Solution().solution(new int[]{1,2,3,4}));
-    }
+//    //F - Binary String Reconstruction https://codeforces.com/contest/1352/problem/F https://codeforces.com/contest/1352/submission/79581910
+//    @Test
+//    public void testAsDescribedInTask2() {
+//        assertEquals(16, Main.solve(3, 9, 3).length());
+//    }
+//
+//    @Test
+//    public void testMyTestNoLeadingZeros3() {
+//        assertEquals(16, Main.solve(0, 2, 3).length());
+//    }
+//
+//    @Test
+//    public void testMyTest1AllSame() {
+//        assertEquals(6, Main.solve(5, 0, 0).length());
+//        assertEquals(6, Main.solve(0, 0, 5).length());
+//    }
+
+
+//    //C - K-th Not Divisible by n https://codeforces.com/contest/1352/problem/C https://codeforces.com/contest/1352/submission/79549060
+//
+//    @Test
+//    public void testAsDescribedInTask() {
+//        assertEquals(10, Main.solve(3, 7));
+//        assertEquals(15, Main.solve(4, 12));
+//        assertEquals(1999999999, Main.solve(2, 1000000000));
+//        assertEquals(113, Main.solve(7, 97));
+//        assertEquals(1, Main.solve(2, 1));
+//        assertEquals(1000000001, Main.solve(1000000000, 1000000000));
+//    }
+//
+//    @Test
+//    public void testAsDescribedInTask2() {
+//        assertEquals(10, Main.solve(3, 7));
+//    }
+
+    //    //B - Same Parity Summands https://codeforces.com/contest/1352/problem/B https://codeforces.com/contest/1352/submission/79525295
+//
+//    @Test
+//    public void testAsDescribedInTask() {
+//        assertNull(Main.solve(8, 7));
+//    }
+//
+//
+//    @Test
+//    public void testAsDescribedInTask3() {
+//        assertNull(Main.solve(97, 2));
+//    }
+//
+//    @Test
+//    public void testAsDescribedInTask2() {
+//        assertFalse(Main.solve(10, 3).isEmpty());
+//    }
+//
+//    @Test
+//    public void testAsDescribedInTask5() {
+//        assertFalse(Main.solve(100, 4).isEmpty());
+//    }
+//
+//
+//    @Test
+//    public void testAsDescribedInTask6() {
+//        assertNull(Main.solve(3, 10));
+//    }
+//
+//    @Test
+//    public void testAsDescribedInTask7() {
+//        assertFalse(Main.solve(5, 3).isEmpty());
+//    }
+//
+//    @Test
+//    public void testAsDescribedInTask8() {
+//        assertFalse(Main.solve(1000000000 , 9).isEmpty());
+//    }
+//
+//    @Test
+//    public void testMyTest1() {
+//        assertFalse(Main.solve(4, 4).isEmpty());
+//    }
+//
+//    @Test
+//    public void testMyTest2() {
+//        assertNull(Main.solve(4, 5));
+//    }
+//
+//    @Test
+//    public void testMyTest3() {
+//        assertNull(Main.solve(3, 5));
+//    }
 
 }
